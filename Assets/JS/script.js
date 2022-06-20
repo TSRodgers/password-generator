9e9487e8ea1b5a6354dbca4032a56a747ef1afae
Tyler <tyler.rodgers74@gmail.com>
2022-06-20 02:33:54 -0400

    wrote function for character type selection

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var passLength = ""
+var charTypes = ""
 
 // Prompts user for length of password and checks if valid input
 function findLength() {
@@ -14,10 +15,53 @@ function findLength() {
     }
 }
 
+// Prompts the user to decide what character types they want to use in pass
+function findTypes() {
+    lowerConfirm = window.confirm("Would you like your password to include lowercase letters?");
+    if (lowerConfirm) {
+      charTypes += "abcdefghijklmnopqrstuvwxyz"
+      upperConfirm = window.confirm("Would you like your password to include uppercase letters?")
+    } else {
+      upperConfirm = window.confirm("Would you like your password to include uppercase letters?")
+    }
+
+    if (upperConfirm) {
+      charTypes += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+      numberConfirm = window.confirm("Would you like your password to include numbers?")
+    } else {
+      numberConfirm = window.confirm("Would you like your password to include numbers?")
+    }
+
+    if (numberConfirm) {
+      charTypes += "1234567890"
+      specialConfirm = window.confirm("Would you like your password to include special characters?")
+    } else {
+      specialConfirm = window.confirm("Would you like your password to include special characters?")
+    }
+
+    if (specialConfirm) {
+      charTypes += " !#$%&'()*+,-./;:<=>?@[]^_`{}|~" + '"'
+    } else {
+    }
+
+    if (charTypes === "") {
+      window.alert("You must select at least one character type, try again.")
+      findTypes(); 
+    } else {
+      console.log(charTypes)
+      return charTypes
+    }
+}
+
+function generatePassword() {
+  
+}
+
 
 // Write password to the #password input
 function writePassword() {
   findLength();
+  findTypes();
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
